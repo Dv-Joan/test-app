f3a135d771a55ca35abd3ce15ff8d27aeb3b40a9
Dv-Joan <joan300501@gmail.com>
2022-05-14 11:28:44 -0500

    Modified

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import UseEffect from './Components/UseEffect';
 import UseState from './Components/UseState';
 import FetchAPI from './Components/FetchAPI';
 import React from 'react';
+import Banner from './Components/banner.png'
 
 
 function App() {
 
-  return (
+  return (  
     <React.Fragment >
-    <div className="text-5xl text-center  font-mono font-bold py-10 px-7 " style={{background : "#F4F3F8" , color: "#201C31"}}>
-      REACT | TailWind BASICS 
+    <div className="text-5xl text-center font-mono font-bold py-10 px-7 " style={{background : "#F4F3F8" , color: "#201C31"}}>
+      <div className="grid justify-items-center">
+      <div >
+        <img className="w-40 pb-2  " src={Banner}/>
+      </div>
+      <div className="">
+      TailWind | React BASICS
+      </div> 
+    </div>
     </div>
     <div className=" text-white py-1  px-11 " style={{background : "#F4F3F8" , color : "#201C31"}}>
     <UseEffect />
-    <UseState />
+    <UseState />  
     <FetchAPI />
     </div>
     
