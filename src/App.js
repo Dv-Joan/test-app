2adfa25d1cebafb89e4625cec6e33310e383da70
Dv-Joan <joan300501@gmail.com>
2022-07-25 14:40:11 -0500

    Events started

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import UseEffect from './Components/UseEffect';
 import UseState from './Components/UseState';
 import FetchAPI from './Components/FetchAPI';
 import React from 'react';
-
+import Events from './Components/Events';
 
 function App() {
-
   return (
-    <React.Fragment >
-    <div className="text-5xl text-center  font-mono font-bold py-10 px-7 " style={{background : "#F4F3F8" , color: "#201C31"}}>
-      REACT | TailWind BASICS 
-    </div>
-    <div className=" text-white py-1  px-11 " style={{background : "#F4F3F8" , color : "#201C31"}}>
-    <UseEffect />
-    <UseState />
-    <FetchAPI />
-    </div>
-    
+    <React.Fragment>
+      <div
+        className='text-5xl text-center  font-mono font-bold py-10 px-7 '
+        style={{ background: '#F4F3F8', color: '#201C31' }}
+      >
+        REACT | TailWind BASICS
+      </div>
+      <div
+        className=' text-white py-1  px-11 '
+        style={{ background: '#F4F3F8', color: '#201C31' }}
+      >
+        <UseEffect />
+        <UseState />
+        <FetchAPI />
+        <Events />
+      </div>
     </React.Fragment>
-    
   );
 }
 
